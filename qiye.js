3578c011814bd8f5723328e2ae47f527707bf3e4
hxhhxrhxh <1527287696@qq.com>
2018-09-06 22:51:05 +0800

    qiye

---
+++ qiye.js
@@ -0,0 +1,28 @@
+/*
+* @Author: Administrator
+* @Date:   2018-09-06 17:44:30
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-09-06 18:09:28
+*/
+// window.onload=function(){
+// 	let flag=true;
+// 	let imgs=document.querySelectorAll(".feed li");
+// 	let dots=document.querySelectorAll(".dots .dot-list")
+// 	let widths=parseInt(getComputedStyle(imgs[0],null).width);
+// 	// console.log(imgs,dots);
+// 	console.log(widths);
+// 	for(let i=0;i<dots.length;i++){
+//     dots[i].onmouseover=function () {
+//         if(flag){
+//             for(let j=0;j<dots.length;j++){
+//                 dots[j].classList.remove("active");
+//                 imgs[j].style.left=-widths+"px";
+//             }
+//             dots[i].classList.add("active");
+//             imgs[i].style.left=0;
+//             now=i;
+//             next=i;
+//         }
+//     }
+// }
+// }
